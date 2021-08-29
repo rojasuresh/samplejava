e2e183770de49ea93906dd155312904080548c7e
rojasuresh <89644191+rojasuresh@users.noreply.github.com>
2021-08-29 07:09:31 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
     class Simple{  
         public static void main(String args[]){  
          System.out.println("Hello Java");  
+             System.out.println("Hello Linux");
+             System.out.println("Hello DevOpps");
+            
         }  
     }  
