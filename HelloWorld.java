8a9edbb72510b654d010b7a73140d287354d6c5e
rojasuresh <89644191+rojasuresh@users.noreply.github.com>
2022-01-22 19:00:43 +0530

    Update HelloWorld.java
    two sop changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@
          System.out.println("Hello Java");  
              System.out.println("Hello Linux");
              System.out.println("Hello DevOpps");
+            System.out.println("Hello DevOpps1");
+            System.out.println("Hello DevOpps2");
+            
             
         }  
     }  
